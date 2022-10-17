82bd73ba111fca4c7eff694800fabc16d080f26a
Jose Paredes - House <gabriel.changed@gmail.com>
2022-10-17 00:12:34 -0400

    fix armandus

--- src/variables/Timeline.jsx
+++ src/variables/Timeline.jsx
@@ -1,162 +1,157 @@
-import Diseñador from '../assets/timeline/diseñadorgrafico.jpg'
-import CreacionMarca from '../assets/timeline/creacionmarca.webp'
-import AutoWeb from '../assets/timeline/autoweb2.png'
-import Rrss from '../assets/timeline/rrss.jpg'
-import Igdaikyoto from '../assets/timeline/instagramdaikyoto2.png'
-import Tendencias from '../assets/timeline/tendencias.png' 
-import Analsitait from '../assets/timeline/analistait.jpg'
-import Trackingrp from '../assets/timeline/trackinrep.jpeg'
-import MonitorTaller from '../assets/timeline/monitortaller.jfif'
-import CambiarPc from '../assets/timeline/cambiarpc.jpeg'
-import LogoBcv from '../assets/timeline/bcv.jpg'
-
+import Diseñador from '../assets/timeline/diseñadorgrafico.jpg';
+import CreacionMarca from '../assets/timeline/creacionmarca.webp';
+import AutoWeb from '../assets/timeline/autoweb2.png';
+import Rrss from '../assets/timeline/rrss.jpg';
+import Igdaikyoto from '../assets/timeline/instagramdaikyoto2.png';
+import Tendencias from '../assets/timeline/tendencias.png';
+import Analsitait from '../assets/timeline/analistait.jpg';
+import Trackingrp from '../assets/timeline/trackinrep.jpeg';
+import MonitorTaller from '../assets/timeline/monitortaller.jfif';
+import CambiarPc from '../assets/timeline/cambiarpc.jpeg';
+import LogoBcv from '../assets/timeline/bcv.jpg';
 
 const timeline = [
 	{
-		title: 'Contratacion de Diseñador Grafico.',
-        contenido: [],
-        img: Diseñador,
+		title: 'Contratación de Diseñadores Gráficos.',
+		contenido: [],
+		img: Diseñador,
 	},
 	{
 		title: 'Creación de Marca “Repuestos Orientales”.',
-        contenido: [
-            'Actualizar Colores Corporativos y Logo.',
-            'Cuentas en RR.SS. Instagram / Facebook / Youtube.',
-            'Creación de cuenta en Mercado Libre.',
-            'Solicitud de Línea Movistar para llevar la marca.',
-            'Compra Teléfono Inteligente para llevar la marca.',
-            'Compra dominio .ve para posicionamiento Nacional.',
-            'Compra dominio .com para posicionamiento Internacional.',
-            'Landing Page de la marca.',
-            'Search Console de "Repuestos Orientales" dominios .com y .ve.',
-            'Correos: ventas@ y compras@.',
-            'Creación whastapp business .',
-        ],
-        img: CreacionMarca,
+		contenido: [
+			'Actualización Colores Corporativos y Logo.',
+			'Cuentas en RR.SS. Instagram / Facebook / Youtube.',
+			'Creación de cuenta en Mercado Libre.',
+			'Solicitud de Línea Movistar para llevar la marca.',
+			'Compra Teléfono Inteligente para llevar la marca.',
+			'Compra dominio .ve para posicionamiento Nacional.',
+			'Compra dominio .com para posicionamiento Internacional.',
+			'Landing Page de la marca.',
+			'Search Console de "Repuestos Orientales" dominios .com y .ve',
+			'Correos: ventas@ y compras@.',
+			'Creación Whatsapp Business.',
+		],
+		img: CreacionMarca,
 	},
-    {
+	{
 		title: 'Creación de Marca “Katay Motors. S.A.”.',
-        contenido: [
-            'Paleta de colores con referencia a las marca Jetour y Karry.',
-            'Vectorizacion de Logos de marca.',
-            'Cuentas en RR.SS. Instagram / Facebook / Youtube.',
-            'Creación de cuenta en Mercado Libre.',
-            'Solicitud de Línea Movistar para llevar la marca.',
-            'Compra Teléfono Inteligente para llevar la marca.',
-            'Compra dominio .ve para posicionamiento Nacional.',
-            'Compra dominio .com para posicionamiento Internacional.',
-            'Landing Page de la marca.',
-            'Search Console de "Katay Motors. S.A." dominios .com y .ve.',
-            'Correos: ventas@ y compras@.',
-            'Creación whastapp business .',
-        ],
-        img: CreacionMarca,
+		contenido: [
+			'Paleta de colores con referencia a las marca Jetour y Karry.',
+			'Vectorización de Logos de marca.',
+			'Cuentas en RR.SS. Instagram / Facebook / Youtube.',
+			'Creación de cuenta en Mercado Libre.',
+			'Solicitud de Línea Movistar para llevar la marca.',
+			'Compra Teléfono Inteligente para llevar la marca.',
+			'Compra dominio .ve para posicionamiento Nacional.',
+			'Compra dominio .com para posicionamiento Internacional.',
+			'Landing Page de la marca.',
+			'Search Console de "Katay Motors. S.A." dominios .com y .ve',
+			'Correos: ventas@ y compras@.',
+			'Creación Whatsapp Business.',
+		],
+		img: CreacionMarca,
 	},
-    {
-		title: 'Finalizar proyecto autoWeb.',
-        contenido: [
-            'Creación del Módulo de Contaduría.',
-            'Creación del Módulo de Unidades.',
-        ],
-        img: AutoWeb,
+	{
+		title: 'Finalización proyecto AutoWeb.',
+		contenido: ['Creación del Módulo de Contaduría.', 'Creación del Módulo de Unidades.'],
+		img: AutoWeb,
 	},
-    {
+	{
 		title: 'Contenido de la marca en RR.SS. “Repuestos Orientales”.',
-        contenido: [
-            'Creacion Estructura de las cuentas.',
-            'Creacion Stikers, Mensajes automáticos.',
-            'Creacion Posts alusivos a la apertura de la marca.',
-        ],
-        img: Rrss
+		contenido: [
+			'Creación de estructura de las cuentas.',
+			'Creación de stickers y Mensajes automáticos.',
+			'Creación de posts alusivos a la apertura de la marca.',
+		],
+		img: Rrss,
 	},
-    {
+	{
 		title: 'Contenido de la marca en RR.SS. “Katay Motors. S.A.”.',
-        contenido: [
-            'Creacion Estructura de las cuentas.',
-            'Creacion Stikers, Mensajes automáticos.',
-            'Creacion Posts alusivos a la apertura de la marca.',
-        ],
-        img: Rrss
+		contenido: [
+			'Creación de estructura de las cuentas.',
+			'Creación de stickers y mensajes automáticos.',
+			'Creacion posts alusivos a la apertura de la marca.',
+		],
+		img: Rrss,
 	},
-    {
-        title: 'Toma de Posición cuenta DaiMotors IG y Kyoto Motors.',
-        contenido: [
-            'Planificación Semanal de ambas marcas.',
-            'Publicación de Repuestos Diarios en IG y Mercado Libre.',
-            'Publicación de Encuestas.',
-            'Presentación de los Vehículos 2022 / 2023.',
-            'Reels de presentación de Repuestos (Contenido Orgánico).',
-            'Stories Diarias de Encuestas y Feedback con Seguidores.',
-            'Planificación Mensual de las marcas.',
-            'Promociones del mes.',
-            'Tendencia de Festividad aplicada a la Cuente.',
-            'Contacto con influencers.',
-            'Publicidad en otras redes e Instagram.',
-        ],
-        img: Igdaikyoto
+	{
+		title: 'Toma de Posición cuenta DaiMotors IG y Kyoto Motors.',
+		contenido: [
+			'Planificación semanal de ambas marcas.',
+			'Publicación de Repuestos diariamente en IG y Mercado Libre.',
+			'Publicación de Encuestas.',
+			'Presentación de Vehículos 2022 / 2023.',
+			'Reels de presentación de Repuestos (Contenido Orgánico).',
+			'Stories Diarias de Encuestas y Feedback con Seguidores.',
+			'Planificación Mensual de las marcas.',
+			'Promociones del Mes.',
+			'Tendencia de Festividad aplicada a la Cuenta.',
+			'Contacto con Influencers.',
+			'Publicidad en otras redes e Instagram.',
+		],
+		img: Igdaikyoto,
 	},
-    {
-        title: 'Implementación de la Tendencia en las Instalaciones.',
-        contenido: [
-            'Fomentar la Información para que todos los Departamentos productivos.',
-            'Publicidad Física en los Departamentos productivos.',
-            'Decoración de las áreas productivas.',
-        ],
-        img: Tendencias
+	{
+		title: 'Implementación de la Tendencia en las Instalaciones.',
+		contenido: [
+			'Fomentación de la información para todos los Departamentos Productivos.',
+			'Publicidad Física en los Departamentos Productivos.',
+			'Decoración de las áreas productivas.',
+		],
+		img: Tendencias,
 	},
-    {
-        title: 'Contratación de Analista de IT Junior.',
-        contenido: [],
-        img: Analsitait
+	{
+		title: 'Contratación de Analistas de IT Junior.',
+		contenido: [],
+		img: Analsitait,
 	},
-    {
-        title: 'Proyecto “Repuestos Tracking”.',
-        contenido: [
-            'App Web y Mobile de Rastreo de Repuestos para Clientes.',
-            'Motor de Programación.',
-            'Diseño de Módulos.',
-            'Diseño en figma.',
-            'Creación de APIs.',
-            'Creación de Front-End.',
-            'Lanzamiento Oficial en Play Store.',
-        ],
-        img: Trackingrp
-    },
-    {
-        title: 'Proyecto “Monitorea Tu Vehículo”.',
-        contenido: [
-            'App Mobile para que los clientes sepan el estatus de sus vehículos en el taller e historial de reparaciones.',
-            'Motor de Programación.',
-            'Diseño de Módulos.',
-            'Diseño Figma.',
-            'Creación de APIs.',
-            'Creación de Front-End.',
-            'Lanzamiento Oficial Play Store.',
-            'Lanzamiento Oficial Apple Store.',
-        ],
-        img: MonitorTaller
-    },
-    {
-		title: 'Migración de Equipos Viejos.',
-        contenido: [
-            'Comprar Mensual/Bimensual un equipo e ir migrando los equipos antiguos.',
-            'Conseguir fondos a través de la venta de equipos antiguos ya sin uso.',
-        ],
-        img: CambiarPc
+	{
+		title: 'Proyecto “Autopartes Tracking”.',
+		contenido: [
+			'App Web y Mobile de Rastreo de Repuestos para Clientes.',
+			'Motor de Programación.',
+			'Diseño de Módulos.',
+			'Diseño en Figma.',
+			'Creación de APIs.',
+			'Creación de Front-End.',
+			'Lanzamiento Oficial en Play Store.',
+		],
+		img: Trackingrp,
 	},
-    {
-        title: 'Proyecto “Tasa BCV”.',
-        contenido: [
-            'App para dejar fija la tasa BCV en Caja con conexión directa a la página del BCV.',
-            'Motor de Programación.',
-            'Módulos.',
-            'Landing Page.',
-            'Compra Tablet.',
-            'Compra Armazón de Tablet.',
-        ],
-        img: LogoBcv,
-    },
-
+	{
+		title: 'Proyecto “Monitorea Tu Vehículo”.',
+		contenido: [
+			'App Mobile de estatus e historial de reparaciones de vehículos en taller para el cliente.',
+			'Motor de Programación.',
+			'Diseño de Módulos.',
+			'Diseño Figma.',
+			'Creación de APIs.',
+			'Creación de Front-End.',
+			'Lanzamiento Oficial Play Store.',
+			'Lanzamiento Oficial Apple Store.',
+		],
+		img: MonitorTaller,
+	},
+	{
+		title: 'Actualización de Equipos Antiguos.',
+		contenido: [
+			'Compra Mensual/Bimensual de equipos e ir actualizando equipos antiguos.',
+			'Capitalización de fondos a través de la venta de equipos antiguos sin uso.',
+		],
+		img: CambiarPc,
+	},
+	{
+		title: 'Proyecto “Tasa BCV”.',
+		contenido: [
+			'App de Web Scapping a la Tasa BCV en Caja con conexión directa a la página del BCV.',
+			'Motor de Programación.',
+			'Módulos.',
+			'Landing Page.',
+			'Compra Tablet.',
+			'Compra Armazón de Tablet.',
+		],
+		img: LogoBcv,
+	},
 ];
 
-export default timeline
+export default timeline;
