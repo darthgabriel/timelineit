0eae0c607d82c06e0a4058880c863f093bfff4c9
Jose Paredes - House <gabriel.changed@gmail.com>
2022-10-17 00:31:43 -0400

    fix palabras

--- src/variables/Organizacion.jsx
+++ src/variables/Organizacion.jsx
@@ -1,37 +1,38 @@
 const organizacion = [
 	{
-		cargo: 'Jefe Dpto',
+		cargo: 'Jefe de Dpto',
 		funciones: [
-			'Planifar, Diseñar y Ejecutar Estrategias con el fin que de la tecnologia sea un aporte a la Organizacion y al correcto desarrollo de la misma',
-			'Administra y desarrolla toda la plataforma tecnologica de la Organizacion',
-			'Controla y refuerza la estabilidad operativa, la continuidad de servicio IT',
-			'Techlead  de Nuevas Apps',
-			'Monitoreo Directo de Marcas, Tendencias y Competencias',
-			'Poner en practica campañas de Marketing que garanticen el exito',
-			'Dirigir las Campañas de Arte y Marketing',
-			'Desarollo de estrategias y tacticas para aumentar la reputacion de la marca',
+			'Planifica, Diseña y Ejecuta estrategias con el fin de que la tecnología sea un aporte a la Organización y al correcto desarrollo de la misma.',
+			'Administra y desarrolla la plataforma tecnológica de la Organización.',
+			'Controla y refuerza la estabilidad operativa y continuidad del servicio de IT.',
+			'Techlead  de nuevas Apps.',
+			'Monitoreo directo de Marcas, Tendencias y Competencias.',
+			'Pone en practica campañas de Marketing que garanticen éxito.',
+			'Dirige campañas de Arte y Marketing.',
+			'Desarolla estrategias y tacticas para aumentar la reputación de la marca.',
 		],
 	},
 	{
 		cargo: 'Diseñador Grafico',
 		funciones: [
-			'Creacion de Contenido visuales de la marca',
-			'Genera contenido: Post, Reels, Whitepaper, Etc',
-			'Generar contenido valioso y atractivo para el sitio web, tienda digital y redes sociales',
-			'Cuidar los estandares audiovisuales y de marca',
-			'Formular planes de proyecto para el diseño de la organizacion'
+			'Creación de contenidos visuales de la marca.',
+			'Genera contenido: Posts, Reels, Whitepapers, Etc...',
+			'Genera contenido valioso y atráctivo para el sitio Web, tienda digital y RR.SS.',
+			'Cuida los estándares audiovisuales de la marca.',
+			'Formula planes de proyecto para el Diseño de la Organización.',
 		],
 	},
 	{
 		cargo: 'Soporte & Programador Web',
-		funciones: ['Creación de soluciones web y Mobile',
-					'Dar Soporte a los distintos requerimientos de los usuarios',
-					'Integrar datos de backend a Frontend',
-					'Identificar Fallas y crear soluciones Informaticas',
-					'Supervisar el funcionamiento correcto de las Aplicaciones de la organizacion',
-					'Instalacion y Mantenimiento del Sistema CCTV de la organizacion',
-				],
+		funciones: [
+			'Creación de soluciones Web y Mobile',
+			'Da soporte a los distintos requerimientos de los usuarios.',
+			'Integra datos de Back-end a Front-end.',
+			'Identifica fallas y crea soluciones Informaticas.',
+			'Supervisa el funcionamiento correcto de las Aplicaciones de la Prganización.',
+			'Instalación y Mantenimiento del Sistema CCTV de la Organización.',
+		],
 	},
 ];
 
-export default organizacion
+export default organizacion;
